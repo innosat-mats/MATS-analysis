0f1f4be7eb61dad04115975408037ded84517b2d
Ceona Lindstein <ceonalindstein@hotmail.com>
2023-02-24 16:56:19 +0100

    New: one can choose vertical or horizontal strip

--- Ceona/Keogram.py
+++ Ceona/Keogram.py
@@ -15,37 +15,47 @@ class CenterStrip:
     def __init__(self, CCDobject, width):
         self.image = CCDobject['IMAGE']
         self.width = width
-        self.strip = self.makestrip()
+        self.strip = []
         self.latitude = TPpos(CCDobject)[0] 
         self.time =  pd.to_datetime(CCDobject['EXPDate'])
 
     "Makes a strip object from the image"
-    def makestrip(self):
-        "Number of columns in the image"
-        #center = math.ceil(len(self.image[0])/2)
-        #self.strip = self.image[:,int(center-self.width/2):int(center+self.width/2)]
-
-        #changed to horizontal strip
+    def makeVerticalStrip(self):
+        "finds the center pixel"
+        center = math.ceil(len(self.image[0])/2)
+        self.strip = self.image[:,int(center-self.width/2):int(center+self.width/2)]
+        return  self.strip
+    
+    def makeHorizontalStrip(self):
         center = math.ceil(len(self.image)/2)
         self.strip = self.image[int(center-self.width/2):int(center+self.width/2),:]
-       
         return  np.transpose(self.strip)
 
 #%%  Make a keogram of a specific channel and CCD-objects
-def makeStripMatrix(df, channel_type, width):
+def makeStripMatrix(df, channel_type, width, strip_dir):
 
     IR_list = df[df['channel'] == channel_type]
     strips_matrix = []  #matrix of concatenated strips
-    listofstrips = []   #list of strip objects
 
-    #iterates through the CCDobjects (each row) and creates a strip
-    for index, row in IR_list.iterrows():
-        new_strip = CenterStrip(row, width)  #creates strip object
-        listofstrips.append(new_strip) 
-        for i in range(width):
-            strips_matrix.append(new_strip.strip[:,i])
-    strips_matrix = np.array(strips_matrix)
-  
+    #creates a matrix from vertical strips
+    if strip_dir == 'v':
+        #iterates through the CCDobjects (each row) and creates a strip
+        for index, row in IR_list.iterrows():
+            new_strip = CenterStrip(row, width) #creates strip object
+            new_strip.makeVerticalStrip()
+            for i in range(width):
+                strips_matrix.append(new_strip.strip[:,i])
+        strips_matrix = np.array(strips_matrix)
+
+    #creates a matrix from horizontal strips
+    if strip_dir== 'h':
+           #iterates through the CCDobjects (each row) and creates a strip
+        for index, row in IR_list.iterrows():
+            new_strip = CenterStrip(row, width)  #creates strip object
+            new_strip.makeHorizontalStrip()
+            for i in range(width):
+                strips_matrix.append(new_strip.strip[:,i])
+        strips_matrix = np.array(strips_matrix)
     return np.transpose(strips_matrix)
     
 def getLatitudes(df, channel_type):
@@ -58,34 +68,48 @@ def getLatitudes(df, channel_type):
     return  listoflatitudes, listofexpdates
 
 
-# %%
-start_time = DT.datetime(2023,2,8,19,15,0)
-stop_time = DT.datetime(2023,2,8,20,30,0)
+ # %% Read data
+start_time = DT.datetime(2023,2,18,18,30,0)
+stop_time = DT.datetime(2023,2,18,22,30,0)
 df = read_MATS_data(start_time,stop_time)
-
 #Can add filter in read Mats data filter = {"TPlat":[-20,20]}
 
-# %%
-width = 1
+# %%  Settings to run
 channels = ['IR1']
+strip_dir = 'v'
+width = 1
 
-# %%
-def plotKeogram(df, channels, width):
-    fig,axs = plt.subplots(nrows=2, ncols=1)
-    #axs = axs.flatten()
+# %% Makes a plot of the matrix made with makeStripMatrix()
+def plotKeogram(df, channels, width, strip_dir):
 
-    #for i,ax in enumerate(axs):
-    i = 0
-    matrix = makeStripMatrix(df,channels[i],width)
-    latitudes, dates = getLatitudes(df,channels[i])
-   
-    axs[0].pcolormesh(dates,range(matrix.shape[0]),matrix)
-    axs[0].set_title(f"Channel {channels[i]}")
-    axs[1].set_xlabel('Time')
-    axs[1].set_ylabel('Latitude')
-    axs[1].plot(dates,latitudes,'.')
-    axs[1].set_xlim(dates[0],dates[-1])
-
+    #gives two plots, one with the matrix and one with the latitudes
+    if len(channels)==1 :
+        fig,axs = plt.subplots(nrows=2, ncols=1)
+        latitudes, dates = getLatitudes(df,channels[0])
+        matrix = makeStripMatrix(df,channels[0],width,strip_dir)
+        axs[0].pcolormesh(dates,range(matrix.shape[0]),matrix)
+        axs[0].set_title(f"Channel {channels[0]}")
+        axs[1].set_xlabel('Time')
+        axs[1].set_ylabel('Latitude')
+        axs[1].plot(dates,latitudes,'.')
+        axs[1].set_xlim(dates[0],dates[-1])
+        axs[1].grid(linestyle='-')
+    else:
+        fig,axs = plt.subplots(nrows=len(channels)+1, ncols=1)
+        latitudes, dates = getLatitudes(df,channels[0])
+        axs[len(channels)].set_xlabel('Time')
+        axs[len(channels)].set_ylabel('Latitude')
+        axs[len(channels)].plot(dates,latitudes,'.')
+        axs[len(channels)].set_xlim(dates[0],dates[-1])
+        for i in range(len(channels)):
+            latitudes, dates = getLatitudes(df,channels[i])
+            print(len(dates))
+            matrix = makeStripMatrix(df,channels[i],width,strip_dir)  
+            axs[i].pcolormesh(dates,range(matrix.shape[0]),matrix)
+            axs[i].set_title(f"Channel {channels[i]}")
+            plt.gcf().autofmt_xdate()
+            plt.tight_layout()
+       
     plt.tight_layout()
     plt.gcf().autofmt_xdate()
     plt.show()
