30360a9649a9613022958d785898806a3405334e
Kernl67 <louis.kern@student.isae-supaero.fr>
2023-05-29 13:34:00 +0200

    adding daily routine

--- Louis/monitoring/monitoring_call.sh
+++ Louis/monitoring/monitoring_call.sh
@@ -4,23 +4,38 @@
 conda activate MATS_analysis
 
 # main path
-MATS_dir='/home/louis/projects/MATS/'
+MATS_dir='/home/louis/MATS/'
 
 
 # output
-outdir=${MATS_dir}'animations/daily/'$dates'/'
+outdir=${MATS_dir}'MATS-Data/Monitoring'
 
-# measurement date
-start_time='2022:4:24_0:0:0'
-stop_time='2022:4:27_0:0:0
+#### Daily data and health monitoring
 
-# initiate
-echo -e "DAILY ANIMATION (${dates}): Initiating ..."
-echo 'MATS DATA: L'${level}' data (v'${version}')..'
-echo 'Generating figures in: '${outdir}
+# daily monitoring
+daily_start='2023:04:24_0:0:0'
+daily_stop='2023:04:24_0:10:0'
+
+# initiate daily monitoring
+echo -e "Daily data and health monitoring: Initiating ..."
 
 # generate figures
-{ python ${MATS_dir}MATS-analysis/Louis/monitoring/monitoring_routine_weekly.py --outdir ${outdir} --start_time ${start_time} --stop_time ${stop_time}; } &
+{ python ${MATS_dir}MATS-analysis/Louis/monitoring/monitoring_routine_daily.py --outdir ${outdir} --start_time ${daily_start} --stop_time ${daily_stop}; } &
+pid=$!
+wait $pid
+
+
+#### Weekly summary generation
+
+# weekly summary 
+weekly_start='2023:04:01_0:0:0'
+weekly_stop='2023:05:01_0:0:0'
+
+# initiate weekly monitoring
+echo -e "Weekly data and health monitoring summary: Initiating ..."
+
+# generate figures
+{ python ${MATS_dir}MATS-analysis/Louis/monitoring/monitoring_routine_weekly.py --outdir ${outdir} --start_time ${weekly_start} --stop_time ${weekly_stop}; } &
 pid=$!
 wait $pid
 
