9c48d751b7da0142939cd70cf2749d2b264b0bd8
Kernl67 <louis.kern@student.isae-supaero.fr>
2023-06-22 15:29:57 +0200

    bash scripts update

--- Louis/monitoring/monitoring.sh
+++ Louis/monitoring/monitoring.sh
@@ -14,12 +14,12 @@ conda activate mats-analysis
 MATS_dir='/home/louis/MATS/'
 
 # output folder for images
-outdir=${MATS_dir}'MATS-Data/Monitoring/test_temp'
+outdir=${MATS_dir}'MATS-Data/Monitoring/test_all'
 
 # monitoring parameters (False by default)
 sampling_period=60 # sampling period in seconds
 show_plots=False # shows interactive matplotlib plots (might be buggy)
-mode=temp # the modes are the following :
+mode=all # the modes are the following :
 # daily : daily summary
 # temp : all temperature data
 # all : all accessible monitoring data
@@ -27,8 +27,8 @@ mode=temp # the modes are the following :
 
 
 # start and stop of the monitoring
-start_time='2023:03:26_12:0:0'
-stop_time='2023:03:27_0:0:0'
+start_time='2023:03:26_13:0:0'
+stop_time='2023:03:26_13:10:0'
 
 
 #### RUNNING SCRIPT ####
--- Louis/monitoring/monitoring_daily.sh
+++ Louis/monitoring/monitoring_daily.sh
@@ -13,40 +13,27 @@ conda activate mats-analysis
 # main path
 MATS_dir='/home/louis/MATS/'
 
+# current date
+day_start=$(date -d "$date -1 days" +'%Y_%m_%d')
+# output folder for images
+outdir=${MATS_dir}'MATS-Data/Monitoring/daily_'${day_start}
+
 # monitoring parameters (False by default)
 sampling_period=60 # sampling period in seconds
-data_processing=True # data generation and processing success rate between levels
-mode=daily # monitoring mode
+mode=daily
 show_plots=False # shows interactive matplotlib plots (might be buggy)
 
+# start and stop of the monitoring
+start_time=$(date -d "$date -1 days" +"%Y:%m:%d_00:00:00")
+stop_time=$(date +"%Y:%m:%d_00:00:00")
 
 
-#### MONITORING ROUTINE ####
+#### RUNNING SCRIPT ####
 
 # initiate monitoring
 echo -e "Data and health monitoring: Initiating ..."
 
-# monitoring the day before
-start_time=$(date -d "$date -1 days" +"%Y:%m:%d_00:00:00")
-stop_time=$(date +"%Y:%m:%d_00:00:00")
-day_start=$(date -d "$date -1 days" +'%Y_%m_%d')
-
-# output folder for images
-outdir=${MATS_dir}'MATS-Data/Monitoring/daily_'${day_start}
-
-{ python ${MATS_dir}MATS-analysis/Louis/monitoring/monitoring_routine.py --outdir ${outdir} --start_time ${start_time} --stop_time ${stop_time} --sampling_period ${sampling_period} --mode ${mode} --show_plots ${show_plots}; } &
-pid=$!
-wait $pid
-
-# monitoring 2 days before
-start_time=$(date -d "$date -2 days" +"%Y:%m:%d_00:00:00")
-stop_time=$(date +"%Y:%m:%d_00:00:00")
-day_start=$(date -d "$date -2 days" +'%Y_%m_%d')
-
-# output folder for images
-outdir=${MATS_dir}'MATS-Data/Monitoring/daily_'${day_start}
-
-{ python ${MATS_dir}MATS-analysis/Louis/monitoring/monitoring_routine.py --outdir ${outdir} --start_time ${start_time} --stop_time ${stop_time} --sampling_period ${sampling_period} --mode ${mode} --show_plots ${show_plots}; } &
+{ python ${MATS_dir}MATS-analysis/Louis/monitoring/monitoring_routine.py --outdir ${outdir} --start_time ${start_time} --stop_time ${stop_time} --sampling_period ${sampling_period} --mode ${mode}; } &
 pid=$!
 wait $pid
 
