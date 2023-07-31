83f03acb7cd55ea72013c1f8fdb36325f065f797
Kernl67 <louis.kern@student.isae-supaero.fr>
2023-07-31 10:17:26 +0200

    better comments changes to include MAC OS users generating output olders if they do not exist already

--- Louis/monitoring/monitoring_daily.sh
+++ Louis/monitoring/monitoring_daily.sh
@@ -1,34 +1,38 @@
 #!/bin/bash
 
 #### DATA AND HEALTH MONITORING ####
+# The following script is used to monitor the health and data of the MATS instrument.
+# It analyzes the last day of data and generates plots as .png images
 
-# activate conda environment ?
+# LINES TO BE CHANGED BY USER #
 
-source /home/louis/miniconda3/etc/profile.d/conda.sh
+# activate conda environment
+source /home/louis/miniconda3/etc/profile.d/conda.sh # path to conda installation
 conda activate mats-analysis
 
-
-#### PARAMETERS ####
-
-# main path
+# path to the MATS folder containing the repositories (MATS-analysis, calibration_data, MATS-data, MATS-L1-processing ...)
 MATS_dir='/home/louis/MATS/'
 
+#### RUNNING SCRIPT #### (no need to change anything except if you use MAC OS or Windows)
 # current date
-day_start=$(date -d "$date -1 days" +'%Y_%m_%d')
-# output folder for images
+day_start=$(date -d "$date -1 days" +'%Y_%m_%d') #For Linux and Windows users
+#day_start=$(date -v -1d +'%Y_%m_%d') #For MAC OS users
+
+# output folder for the generated images
 outdir=${MATS_dir}'MATS-Data/Monitoring/daily_'${day_start}
 
-# monitoring parameters (False by default)
+# monitoring parameters
 sampling_period=60 # sampling period in seconds
 mode=daily
 show_plots=False # shows interactive matplotlib plots (might be buggy)
 
 # start and stop of the monitoring
-start_time=$(date -d "$date -1 days" +"%Y:%m:%d_00:00:00")
+start_time=$(date -d "$date -1 days" +"%Y:%m:%d_00:00:00") #For Linux and Windows users
+#start_time=$(date -v -1d +"%Y:%m:%d_00:00:00") #For MAC OS users
 stop_time=$(date +"%Y:%m:%d_00:00:00")
 
 
-#### RUNNING SCRIPT ####
+#### RUNNING  PYTHON SCRIPT ####
 
 # initiate monitoring
 echo -e "Data and health monitoring: Initiating ..."
