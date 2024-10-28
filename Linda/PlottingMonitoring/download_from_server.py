bf5eb7f74e159a8b5e259e6d21744b15d1f6a085
lindamegner <linda@misu.su.se>
2024-10-28 18:17:52 +0100

    added filter

--- Linda/PlottingMonitoring/download_from_server.py
+++ Linda/PlottingMonitoring/download_from_server.py
@@ -7,7 +7,11 @@ from mats_utils.plotting.plotCCD import plot_image
 #%%
 start = DT.datetime(2023, 2, 11, 12, 0, 0)
 stop= DT.datetime(2023, 2, 11, 15, 0, 0)
-df=load_multi_parquet('/home/linda/MATS_data/CROPD_v0.6',start, stop)
+#df=load_multi_parquet('/home/linda/MATS_data/CROPD_v0.6',start, stop)
+filter_UV2={'CCDSEL': [6,6]} 
+
+df=load_multi_parquet('../../../data/CROPD_v0.6',start, stop, filt=filter_UV2)
+
 
 datafolder='/home/linda/data/tmp'
 for index, CCD in df[:5].iterrows():
